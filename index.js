fffc55c492595660ffb57431f58c2ca70714b6e7
Fabiola <fabiola.aurelien@gmail.com>
2026-03-20 14:12:00 -0400

    added eventlistener for task name

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 let arrayofTasks = [];
-let taskName = document.getElementById("taskItem");
-let addTaskItemButton = document.getElementById("addTaskItemButton");
-let removeItemButton = document.getElementById("removeItemButton");
-let cartList = document.getElementById("cart");
+let taskInput = document.getElementById("taskItem");
+let addTaskItem = document.getElementById("addTaskItemButton");
+let taskList = document.getElementById("taskList");
 
-let taskItem = {
-    taskName : "Call Doctor",
-    taskCategory: "Work, Home, Family, Health&Wellness",
-    taskDueBy: "date"
-    taskStatus: "In Progress, Completed, Overdue",
+// let taskItem = {
+//     // taskName: "Call Doctor",
+//     // taskCategory: "Work, Home, Family, Health&Wellness",
+//     // taskDueBy: "date",
+//     // taskStatus: "In Progress, Completed, Overdue",
 
-    filterTasks: function(){
-       //filter tasks by status or category
+//     // filterTasks: function () {
+//     //     //filter tasks by status or category
+
+//     renderTaskList()
+//     }
+// };
+
+//
+addTaskItemButton.addEventListener("click", function () {
+    let taskValue = taskInput.value;
+
+    if (taskValue === "") {
+        alert("Please enter an item.");
+        return;
     }
-};
 
+    //checkDuplicateItem(item);
+    arrayofTasks.push(taskValue);
+    renderTaskList();
+    taskValue = ""; // Clear the input field
+});
+
+
+function renderTaskList() {
+    taskList.innerHTML = ""; // Clear existing list
+    for (let i = 0; i < arrayofTasks.length; i++) {
+        let taskListItem = document.createElement("li");
+        taskListItem.innerText = arrayofTasks[i];
+        taskList.appendChild(taskListItem);
+    }
+}

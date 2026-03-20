3c0c4dfd092555e8c860951be8a39d90a5a57dbe
Fabiola <fabiola.aurelien@gmail.com>
2026-03-20 12:07:42 -0400

    Created index.js file with loose outline of taskItem object defined

---
+++ index.js
@@ -0,0 +1,11 @@
+let taskItem = {
+    taskName : "Call Doctor",
+    taskCategory: "Work, Home, Family, Health&Wellness",
+    taskDeadline: "date"
+    taskStatus: "In Progress, Completed, Overdue",
+
+    filterTasks: function(){
+        filter tasks by status or category
+    }
+};
+

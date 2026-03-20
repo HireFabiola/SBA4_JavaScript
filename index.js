4ea5328416e61f5941eedc37a504bc2366a40a07
Fabiola <fabiola.aurelien@gmail.com>
2026-03-20 12:29:21 -0400

    forgot to add. recommitting for same reason as above

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+let arrayofTasks = [];
+let itemInput = document.getElementById("itemInput");
+let addItemButton = document.getElementById("addItemButton");
+let removeItemButton = document.getElementById("removeItemButton");
+let cartList = document.getElementById("cart");
+
 let taskItem = {
     taskName : "Call Doctor",
     taskCategory: "Work, Home, Family, Health&Wellness",
-    taskDeadline: "date"
+    taskDueBy: "date"
     taskStatus: "In Progress, Completed, Overdue",
 
     filterTasks: function(){
-        filter tasks by status or category
+       //filter tasks by status or category
     }
 };
 

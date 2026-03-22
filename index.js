b822fe24fa6a024b5fc4c7bfea8c13f84b5f7ea1
Fabiola <fabiola.aurelien@gmail.com>
2026-03-22 07:53:17 -0400

    updated both files to output task list in rows and columns rather than a single line

--- index.js
+++ index.js
@@ -45,18 +45,60 @@ function renderTaskList() {
     currentDate.setHours(0, 0, 0, 0);
 
     taskList.innerHTML = ""; // Clear existing list
-    
+
     for (let i = 0; i < arrayofTasks.length; i++) {
-        let taskListItem = document.createElement("li");
+        // let taskListItem = document.createElement("li");
+        let taskListItem = arrayofTasks[i];
+
         let dueDate = new Date(arrayofTasks[i].deadline + "T00:00:00");
         dueDate.setHours(0, 0, 0, 0);
-        if ((dueDate < currentDate)&&(arrayofTasks[i].progress !== "Completed")) {
-            taskListItem.innerText = arrayofTasks[i].name_description + " " + arrayofTasks[i].category + " " + arrayofTasks[i].deadline + " " + arrayofTasks[i].progress + " Task is overdue";
-        }
-        else {
-            taskListItem.innerText = arrayofTasks[i].name_description + " " + arrayofTasks[i].category + " " + arrayofTasks[i].deadline + " " + arrayofTasks[i].progress;
-        }
-        taskList.appendChild(taskListItem);
+
+        //if ((dueDate < currentDate) && (arrayofTasks[i].progress !== "Completed")) {
+        //     taskListItem.innerText = arrayofTasks[i].name_description + " " + arrayofTasks[i].category + " " + arrayofTasks[i].deadline + " " + arrayofTasks[i].progress + " Task is overdue";
+        // }
+        // else {
+        //taskListItem.innerText = arrayofTasks[i].name_description + " " + arrayofTasks[i].category + " " + arrayofTasks[i].deadline + " " + arrayofTasks[i].progress;
+        // }
+        // taskList.appendChild(taskListItem);
+
+        // Create row
+        let row = document.createElement("div");
+        row.className = "row w-100 mb-2 border-bottom pb-2";
+
+        // Create columns
+        let colDesc = document.createElement("div");
+        colDesc.className = "col-2";
+        colDesc.innerText = taskListItem.name_description;
+
+        let colCategory = document.createElement("div");
+        colCategory.className = "col-2";
+        colCategory.innerText = taskListItem.category;
+
+        let colDeadline = document.createElement("div");
+        colDeadline.className = "col-2";
+        colDeadline.innerText = taskListItem.deadline;
+
+        let colStatus = document.createElement("div");
+        colStatus.className = "col-2";
+        colStatus.innerText = taskListItem.progress;
+
+        let colFilter = document.createElement("div");
+        colFilter.className = "col-2";
+        colFilter.innerText = ""; // placeholder for later
+
+        let colMessage = document.createElement("div");
+        colMessage.className = "col-2";
+
+        // Append columns to row
+        row.appendChild(colDesc);
+        row.appendChild(colCategory);
+        row.appendChild(colDeadline);
+        row.appendChild(colStatus);
+        row.appendChild(colFilter);
+        row.appendChild(colMessage);
+
+        taskList.appendChild(row)
+
     }
 }
 
